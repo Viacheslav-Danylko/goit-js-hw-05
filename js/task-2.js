ecdb5ed8f1e0cef86a9e44b121ef1a88ec94ec29
Viacheslav Danylko <gamerdvv@gmail.com>
2024-08-11 14:10:56 +0200

    fix code to task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const getUsersWithFriend = (users, friendName) => {
-  return users
-    .filter(user => user.friends.includes(friendName))
-    .map(user => user.name);
+const getUsersWithFriend = (allUsers, friendName) => {
+  return allUsers
+  .filter(user => user.friends.includes(friendName))
+  .map(user => ({ name: user.name, friends: user.friends}));
 };
 
 const allUsers = [
